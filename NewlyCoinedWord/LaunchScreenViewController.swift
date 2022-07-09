e56ee607c137ebe26d862881e6c91130f0489e14
jaehoon <sjh22058@gmail.com>
2022-07-09 16:24:48 +0900

    Initial Commit

---
+++ NewlyCoinedWord/LaunchScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchScreenViewController.swift
+//  NewlyCoinedWord
+//
+//  Created by Seo Jae Hoon on 2022/07/09.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        sleep(3)
+        
+        
+    }
+
+}
---
+++ NewlyCoinedWord/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  NewlyCoinedWord
+//
+//  Created by Seo Jae Hoon on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var ResultLabel: UILabel!
+    @IBOutlet weak var SearchTextField: UITextField!
+    
+    
+    @IBOutlet var HastagButtons: [UIButton]!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+    
+    
+    
+    //돋보기 버튼이 눌렸을 때
+    @IBAction func SearchButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    
+    
+    //추가사항
+    //1. textField, searchButton, resultlabel design
+    //2. outletCollectin design
+    //3. hastag button IBAction Code 짜기
+    //4. textField keyboard 내려가는 tap gesture 추가
+    //til 작성
+}
+
