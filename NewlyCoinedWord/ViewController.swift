7c8247173148fbfb704ab3dfaded30a0a8cd5fd0
jaehoon <sjh22058@gmail.com>
2022-07-13 17:34:21 +0900

    code 수정 :  검색어 마지막 버튼에 추가 기능

--- NewlyCoinedWord/ViewController.swift
+++ NewlyCoinedWord/ViewController.swift
@@ -10,37 +10,38 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
 
     
-    @IBOutlet weak var ResultLabel: UILabel!
-    @IBOutlet weak var SearchTextField: UITextField! { didSet { SearchTextField.delegate = self} }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var textField: UITextField! { didSet { textField.delegate = self} }
+    @IBOutlet var hastagButtons: [UIButton]!
     
     
-    @IBOutlet var HastagButtons: [UIButton]!
+    var newWord : [String : String] = [        "윰차" : "유무차별의 줄임말, 구독자 수의 많고 적음을 뜻"
+                                            ,"실매" : "실시간 매니저의 줄임말"
+                                            ,"만반잘부" : "만나서 반가워, 잘 부탁해의 줄임말"
+                                            ,"꾸안꾸" : "꾸민 듯 안꾸민 듯의 줄임말"
+                                            ,"억텐" : "억지 텐션의 줄임말"
+                                            ,"당모치" : "당연히 모든 치킨은 옳다의 줄임말"
+                                            ,"점메추" : "점심메뉴 추천의 줄임말"
+                                            ,"700" : "ㄱㅇㅇ (귀여워)의 초성을 표현한 숫자"
+                                            ,"우유남" : "우월한 유전자를 가진 남자의 줄임말"
+                                            ,"나일리지" : "나이 + 마일리지의 뜻으로 주로 꼰대를 의미"]
     
     
-    var myDictionary : KeyValuePairs = ["윰차":"유무차별의 줄임말, 구독자 수의 많고 적음을 뜻"
-                                          , "실매" : "실시간 매니저의 줄임말"
-                                          , "만반잘부" : "만나서 반가워, 잘 부탁해의 줄임말"
-                                          , "꾸안꾸" : "꾸민 듯 안꾸민 듯의 줄임말"
-                                          , "억텐" : "억지 텐션의 줄임말"
-                                          , "당모치" : "당연히 모든 치킨은 옳다의 줄임말"
-                                          , "점메추" : "점심메뉴 추천의 줄임말"
-                                          , "700" : "ㄱㅇㅇ (귀여워)의 초성을 표현한 숫자"
-                                          , "우유남" : "우월한 유전자를 가진 남자의 줄임말"
-                                          , "나일리지" : "나이 + 마일리지의 뜻으로 주로 꼰대를 의미"]
-    
-    var hastagButtonNums = [0, 1, 2, 3]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        designlabel(labelName: ResultLabel)
-        designTextField(textFieldName: SearchTextField)
-        designButton(buttonName: HastagButtons, hasNum: hastagButtonNums)
+        designlabel(labelName: resultLabel)
+        designTextField(textFieldName: textField)
+        designButton(buttonName: hastagButtons)
         
+        //버튼 초기화
+        hastagButtons[0].setTitle("윰차", for: .normal)
+        hastagButtons[1].setTitle("실매", for: .normal)
+        hastagButtons[2].setTitle("만반잘부", for: .normal)
+        hastagButtons[3].setTitle("꾸안꾸", for: .normal)
         
-        for i in 0...HastagButtons.count - 1 {
-            HastagButtons[i].tag = i
-        }
         
         
     }
@@ -64,72 +65,45 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
     }
     
-    
-    
-    func designButton(buttonName: [UIButton], hasNum: [Int]){
+    func designButton(buttonName: [UIButton]){
 
-        for i in 0...(HastagButtons.count - 1) {
+        for item in hastagButtons {
             
-            HastagButtons[i].setTitle(myDictionary[hastagButtonNums[i]].key, for: .normal)
-            HastagButtons[i].setTitleColor(.black, for: .normal)
-            HastagButtons[i].titleLabel?.font = UIFont.systemFont(ofSize: 15)
-            HastagButtons[i].backgroundColor = .systemBackground
-            HastagButtons[i].layer.cornerRadius = 4
-            HastagButtons[i].layer.borderColor = UIColor.black.cgColor
-            HastagButtons[i].layer.borderWidth = 1
+            item.setTitleColor(.black, for: .normal)
+            item.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+            item.backgroundColor = .systemBackground
+            item.layer.cornerRadius = 4
+            item.layer.borderColor = UIColor.black.cgColor
+            item.layer.borderWidth = 1
         }
 
     }
 
+    //버튼 타이틀 새로 설정
+    func resetButton(button: [UIButton], itemName: String){
+        
+        for i in 0...2{
+        button[i].setTitle(button[i+1].currentTitle, for: .normal)
+        }
+        button[3].setTitle(itemName, for: .normal)
+    }
     
-    
+    // 버튼을 누르면 의미 보여주기
     @IBAction func hastagButtonTapped(_ sender: UIButton) {
-        
-        switch sender.tag {
-        case 0:
-            ResultLabel.text = myDictionary[sender.tag].value
-        case 1:
-            ResultLabel.text = myDictionary[sender.tag].value
-        case 2:
-            ResultLabel.text = myDictionary[sender.tag].value
-        case 3:
-            ResultLabel.text = myDictionary[sender.tag].value
-        default:
-            ResultLabel.text = "모르는 단어입니다."
-        }
-    }
-    
-    func whatWasUsed(_ myDictNum: Int) -> [Int]{
-        
-        hastagButtonNums.remove(at: 0)
-       // print(hastagButtonNums)
-        hastagButtonNums.insert(myDictNum, at: hastagButtonNums.endIndex)
-        
-        return hastagButtonNums
+        guard let meaning = sender.currentTitle else { return }
+        resultLabel.text = newWord[meaning]
     }
     
     
-    //돋보기 버튼이 눌렸을 때 (신조어의 뜻 ResultLabel에 표현)
+    //돋보기 버튼이 눌렸을 때 (신조어의 뜻 ResultLabel에 표현) + button 마지막 label 바꾸는 함수
     @IBAction func SearchButtonTapped(_ sender: UIButton) {
         
-        
-        guard let itemName = SearchTextField.text else { return }
-        
-        for (key, value) in myDictionary {
-            if itemName == key {
-                ResultLabel.text = value
-            }
-            
-            for i in 0...(myDictionary.count - 1) {
-                if itemName == myDictionary[i].key {
-                    ResultLabel.text = myDictionary[i].value
-                    designButton(buttonName: HastagButtons, hasNum: whatWasUsed(i))
-                    //print(whatWasUsed((i)))
-                }
-            }
-        }
+        guard let itemName = textField.text else { return }
+        resultLabel.text = newWord[itemName]
+        resetButton(button: hastagButtons, itemName: itemName)
     }
     
+    //tap제스쳐
     @IBAction func tapGestureTapped(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
